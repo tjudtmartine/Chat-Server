59423e2b3407eb7c7d3fa8f46b47a6fcf426a659
tjudtmartine <145167310+tjudtmartine@users.noreply.github.com>
2024-04-29 12:13:33 -0700

    Update chatserver.py

--- chatserver.py
+++ chatserver.py
@@ -4,37 +4,98 @@
 
 import socket
 import threading
+import sys
 
-def send_out(client_socket, client_list, data):
-    for client_socket in client_list:
-        client_socket.send(data)
-    
-def client_connect(client_socket, client_list):
+def client_in(client):
     while True:
-        data = client_socket.recv(1024)
-        send_out(client_socket, client_list, data)
+        try:
+            message = client.recv(1024).decode('ascii')
+            if not message:
+                print("Connection closed by server.")
+                break
+            print(message)
+        except ConnectionResetError:
+            print("Connection to server lost.")
+            break
 
+def client_out(client):
+    while True:
+        message = input('==> ')
+        client.send(message.encode('ascii'))
+
+def join_server(client, username):
+    join_message = f"JOIN {username}"
+    client.send(join_message.encode('ascii'))
+    print(f"{username} joined!")
+
+def list_users(client):
+    list_message = "LIST"
+    client.send(list_message.encode('ascii'))
+
+def send_message(client, recipient, message):
+    mesg_message = f"MESG {recipient} {message}"
+    client.send(mesg_message.encode('ascii'))
+
+def broadcast_message(client, message):
+    bcst_message = f"BCST {message}"
+    client.send(bcst_message.encode('ascii'))
+
+def quit_server(client):
+    quit_message = "QUIT"
+    client.send(quit_message.encode('ascii'))
 
 def Main():
+    if (len(sys.argv) != 3):
+        print("Usage: python chatclient.py <hostname> <svr_port>")
+        sys.exit(1)
+    host = sys.argv[1]
+    port = int(sys.argv[2])
 
-    host = '127.0.0.1'
-    port = 5000
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((host, port))
 
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((host, port))
-    server.listen()
-    client_list = []
-
+    # Prompt user to enter their username
     while True:
-        print('Server is listening...')
-        client_socket, address = server.accept()
-        print(f'Connected to : {str(address)}')
-        client_list.append(client_socket)
-        thread = threading.Thread(target=client_connect, args=(client_socket, client_list))
-        thread.start()
-    server.close()
+        username_input = input("Enter JOIN followed by your username (e.g., JOIN Mike): ").split()
+        if len(username_input) == 2 and username_input[0].upper() == 'JOIN':
+            username = username_input[1]
+            break
+        else:
+            print("Invalid format. Please enter in the format: JOIN <username>")
 
-if __name__ == "__main__":
+    join_server(client, username)
+    
+    threadIn = threading.Thread(target=client_in, args=(client,))
+    threadOut = threading.Thread(target=client_out, args=(client,))
+
+    threadIn.start()
+    threadOut.start()
+    
+    while True:
+        command = input("Enter command (LIST, MESG, BCST, QUIT): ").split()
+        if command[0] == 'LIST':
+            list_users(client)
+        elif command[0] == 'MESG':
+            if len(command) >= 3:
+                recipient = command[1]
+                message = ' '.join(command[2:])
+                send_message(client, recipient, message)
+            else:
+                print("Invalid MESG command format. Use: MESG <recipient> <message>")
+        elif command[0] == 'BCST':
+            if len(command) >= 2:
+                message = ' '.join(command[1:])
+                broadcast_message(client, message)
+            else:
+                print("Invalid BCST command format. Use: BCST <message>")
+        elif command[0] == 'QUIT':
+            quit_server(client)
+            break
+        else:
+            print("Invalid command!")
+
+    client.close()
+
+if __name__ == '__main__':
     Main()
-
 
