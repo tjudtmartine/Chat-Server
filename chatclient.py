97b7fd02d3ca4914d18f69cf9bd1ed7584b34323
tjudtmartine <145167310+tjudtmartine@users.noreply.github.com>
2024-04-29 15:05:20 -0700

    Update chatclient.py

--- chatclient.py
+++ chatclient.py
@@ -11,29 +11,14 @@ def client_in(client):
     while True:
         try:
             message = client.recv(1024).decode('ascii')
-            if not message:
-                print("Connection closed by server.")
+            if message:
+                print(message)
+            else:
+                print("Error")
                 break
-            print(message)
         except ConnectionError:
             print("Connection to server lost.")
             break
-
-def quit_server(client):
-    quit_message = "QUIT"
-    client.send(quit_message.encode('ascii'))
-
-
-def client_out(client):
-	while True:
-		message = input('==> ')
-		command = message.split()
-		if command[0] == 'QUIT':
-			quit_server(client)
-			#client.close()
-			break
-		else:
-			client.send(message.encode('ascii'))
 
 def Main():
 	if (len(sys.argv) != 3):
@@ -45,27 +30,27 @@ def Main():
 	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	client.connect((host, port))
 
-    # Prompt user to enter their username
-	while True:
-		username_input = input("Enter JOIN followed by your username (e.g., JOIN Mike): ")
-		inputSplit = username_input.split()
-		if len(inputSplit) == 2 and inputSplit[0].upper() == 'JOIN':
-			client.send(username_input.encode())
-			break
-		else:
-			print("Invalid format. Please enter in the format: JOIN <username>")
+	thread = threading.Thread(target=client_in, args=(client,))
 
-	threadIn = threading.Thread(target=client_in, args=(client,))
-	threadOut = threading.Thread(target=client_out, args=(client,))
+	thread.start()
 
-	threadIn.start()
-	threadOut.start()
-
-	threadIn.join()
-	threadOut.join()
-
-	client.close()
-	sys.exit(1)
+	try:
+		print("To join server please enter JOIN <username>")
+		while True:
+			user_input = input("==>")
+			input_split = user_input.split()
+			if len(input_split) == 2 and input_split[0].upper() == 'JOIN':
+				username = input_split[1]
+				print(f"Welcome {username}!")
+			if input_split[0].upper() == 'QUIT':
+				client.send(input_split[0].upper().encode('ascii'))
+				print(f"{username} left the chat!")
+				break
+			else:
+				client.send(user_input.encode())
+	finally:
+		client.close()
+		sys.exit(1)
 
 if __name__ == '__main__':
     Main()
