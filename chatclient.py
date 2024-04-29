fbbc35b4b8c477f0bf1de7ec22bb28ee859f56ba
nazarpot <nazarpot@gmail.com>
2024-04-29 13:21:42 -0700

    fixed port issue

--- chatclient.py
+++ chatclient.py
@@ -50,7 +50,7 @@ def Main():
 		print("Usage: python chatclient.py <hostname> <svr_port>")
 		sys.exit(1)
 	host = sys.argv[1]
-	port = sys.argv[2]
+	port = int(sys.argv[2])
 
 	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	client.connect((host, port))
